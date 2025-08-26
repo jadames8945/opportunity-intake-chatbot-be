d8a31f6105c9765cf1fb1e9a11857596b21d5798
jadames <juan.adames@giantmachines.com>
2025-08-26 10:23:08 -0400

    Fixing app port

--- app/config/app_config.py
+++ app/config/app_config.py
@@ -1,18 +1,11 @@
 import os
-from typing import cast, Optional
+from typing import cast
 
-from dotenv import load_dotenv
-from pydantic_settings import BaseSettings
-
-load_dotenv()
-
-
-class AppConfig(BaseSettings):
-    ENV: str = cast(str, os.getenv("ENV", "DEV"))
-    PORT: int = int(os.getenv("PORT", "8998"))
-    DEBUGGER: Optional[str] = cast(Optional[str], os.getenv("DEBUGGER", None))
-    DEBUGGER_PORT: int = int(os.getenv("DEBUGGER_PORT", "5678"))
-
+class AppConfig:
+    def __init__(self):
+        self.ENV = cast(str, os.getenv("ENV", "DEV"))
+        self.PORT = int(os.getenv("PORT", "8888"))
+        
     def allowed_origins(self):
         env_origins = os.getenv("ALLOWED_ORIGINS")
         if env_origins:
