d1af25b7b34feaeb6dd2e2b55d49666da9332836
jadames <juan.adames@giantmachines.com>
2025-08-27 13:26:53 -0400

    removing auth from docker compose in attempt to get auth to work in coolify

--- app/apis/api_router.py
+++ app/apis/api_router.py
@@ -5,7 +5,7 @@ from app.apis.plan.router import router as plan_router
 from app.apis.upload.router import router as upload_router
 from app.apis.chat_history.router import router as chat_history_router
 from app.apis.airtable.router import router as airtable_router
-
+from auth.apis.auth.router import auth_router
 api_router = APIRouter()
 
 api_router.include_router(plan_router)
@@ -13,3 +13,4 @@ api_router.include_router(upload_router)
 api_router.include_router(healthcheck_router)
 api_router.include_router(chat_history_router)
 api_router.include_router(airtable_router)
+api_router.include_router(auth_router)
